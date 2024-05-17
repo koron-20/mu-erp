a1319b53577b326e75b02d802972bdab6a768feb
Koron Chowdhury <koronchy20@gmail.com>
2024-05-17 22:56:05 +0600

    Student dashboard

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+module.exports = {
+  //...
+  daisyui: {
+    themes: ["light"],
+  },
+}
