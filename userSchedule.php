2a2d53ad29c1eacc55eccd52b61692837c074bbf
Koron Chowdhury <koronchy20@gmail.com>
2024-05-23 12:15:12 +0600

    sure final push

--- userSchedule.php
+++ userSchedule.php
@@ -62,7 +62,7 @@
           <input class="rounded-lg" type="text" name="id" id="id" placeholder="Spring-2024 " required>
       </div>
       <div>
-       <button class="flex items-center text-center bg-green-500 hover:bg-green-700 text-white font-semibold p-2 pr-3 rounded-2xl shadow-2xl ml-32" type="submit" name="submit" id="submit" >
+       <button class="flex items-center text-center bg-green-500 hover:bg-green-700 text-white font-semibold p-2 pr-3 rounded-2xl shadow-2xl ml-28" type="submit" name="submit" id="submit" >
         <svg class="w-10 h-7" data-slot="icon" fill="none" stroke-width="1.5" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
           <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 14.25v-2.625a3.375 3.375 0 0 0-3.375-3.375h-1.5A1.125 1.125 0 0 1 13.5 7.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H8.25m.75 12 3 3m0 0 3-3m-3 3v-6m-1.5-9H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 0 0-9-9Z"></path>
         </svg> Load Schedule
